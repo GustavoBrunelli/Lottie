87fdef425049d160a639ba27446e40493a3199c6
GustavoBrunelli <106496136+GustavoBrunelli@users.noreply.github.com>
2023-08-02 01:06:29 -0300

    a

--- app.py
+++ app.py
@@ -6,7 +6,4 @@ def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 lottie_streamlit = load_lottiefile("./Streamlit Logo Animation.json")
-with st.sidebar:
-    speed = 1
-    reverse = False
-st.lottie(lottie_streamlit, speed=speed, reverse=reverse, height=400, key="initial")
+st.lottie(lottie_streamlit, height=400, key="initial")
