a4364f0b5b9d7d7d8c234a264c4729073d339313
GustavoBrunelli <106496136+GustavoBrunelli@users.noreply.github.com>
2023-08-02 01:04:05 -0300

    a

--- app.py
+++ app.py
@@ -8,16 +8,8 @@ from streamlit_lottie import st_lottie_spinner
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
-
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
 lottie_streamlit = load_lottiefile("./Streamlit Logo Animation.json")
-
 with st.sidebar:
-    speed = st.slider("Select speed", 0.1, 2.0, 1.0)
-    reverse = st.checkbox("Reverse direction", False)
+    speed = 1
+    reverse = False
 st.lottie(lottie_streamlit, speed=speed, reverse=reverse, height=400, key="initial")
