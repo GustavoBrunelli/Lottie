44ed2aa0d2b31b3999e85e43809a8f4a52971b16
GustavoBrunelli <106496136+GustavoBrunelli@users.noreply.github.com>
2023-08-02 01:07:29 -0300

    a

--- app.py
+++ app.py
@@ -5,5 +5,6 @@ from streamlit_lottie import st_lottie
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
+
 lottie_streamlit = load_lottiefile("./Streamlit Logo Animation.json")
-st.lottie(lottie_streamlit, height=400, key="initial")
+st_lottie(lottie_streamlit, height=400, key="initial")
